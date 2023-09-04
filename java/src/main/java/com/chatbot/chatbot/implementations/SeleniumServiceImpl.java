f149be298f443f994674f0241f4caa25a17d0204
Filip Kocovic <filip.kocovic@hpe.com>
2023-09-04 15:35:58 +0200

    Selenium Service Refactor

--- java/src/main/java/com/chatbot/chatbot/implementations/SeleniumServiceImpl.java
+++ java/src/main/java/com/chatbot/chatbot/implementations/SeleniumServiceImpl.java
@@ -29,101 +29,43 @@ public class SeleniumServiceImpl implements SeleniumService {
     private static final String EMPLOYEE_CELL_LOCALE = "lokal_nastavnika";
     private static final String EMPLOYEE_CELL_EMAIL = "mail_nastavnika";
     private static final String EMPLOYEE_CELL_CONSULTATION = "konsultacije_nastavnika";
+    private static final String PARTIAL_LINK_YEAR = " godina";
+    private static final String PARTIAL_LINK_SEMESTER = " semestar";
+    private static final String HREF_TAG = "href";
+
     private static final List<String> OAS_LIST = Arrays.asList("OAS MATEMATIKE", "OAS INFORMATIKE");
     private static final List<String> MAS_LIST = Arrays.asList("MAS MATEMATIKE", "MAS INFORMATIKE");
+    private WebDriver webDriver;
 
-    private void setUp() {
+    private void setUp(String url) {
         WebDriverManager.firefoxdriver().setup();
         FirefoxOptions options = new FirefoxOptions();
         webDriver = new FirefoxDriver(options);
+        webDriver.get(url);
     }
-
-    private WebDriver webDriver;
 
     @Override
     @Cacheable("employees")
     public List<Employee> getAllEmployees() {
-
-        List<Employee> employees = new ArrayList<>();
-
-        setUp();
-        webDriver.get(EMPLOYEE_URL);
-
+        setUp(EMPLOYEE_URL);
         List<WebElement> employeeRows = webDriver.findElements(By.className(EMPLOYEE_ROW_CLASS));
-
-        for (WebElement employeeRow : employeeRows) {
-            employees.add(
-                    Employee.builder().name(getCellPropertyByClassName(employeeRow, EMPLOYEE_CELL_NAME))
-                    .title(getCellPropertyByClassName(employeeRow, EMPLOYEE_CELL_TITLE))
-                    .cabinet(getCellPropertyByClassName(employeeRow, EMPLOYEE_CELL_CABINET))
-                    .locale(getCellPropertyByClassName(employeeRow, EMPLOYEE_CELL_LOCALE))
-                    .email(getCellPropertyByClassName(employeeRow, EMPLOYEE_CELL_EMAIL))
-                    .consultation(getCellPropertyByClassName(employeeRow, EMPLOYEE_CELL_CONSULTATION))
-                    .build()
-            );
-        }
-        webDriver.quit();
-
-        return employees;
+        return extractEmployees(employeeRows);
     }
 
     @Override
     @Cacheable("courses")
     public List<Course> getAllCourses() {
-
-        List<Course> courses = new ArrayList<>();
-
-        List<String> elementaryUrls = new ArrayList<>();
-        List<String> masterUrls = new ArrayList<>();
-        List<String> elementaryYearsUrls = new ArrayList<>();
-        List<String> elementarySemesterUrls = new ArrayList<>();
         List<String> elementaryClassUrls = new ArrayList<>();
 
-        setUp();
-        webDriver.get(MOODLE_URL);
+        setUp(MOODLE_URL);
 
-        for(String oas : OAS_LIST){
-            WebElement course = webDriver.findElement(By.linkText(oas));
-            elementaryUrls.add(course.getAttribute("href"));
-        }
+        List<String> elementaryUrls = getDepartmentUrls(OAS_LIST);
+        List<String> masterUrls = getDepartmentUrls(MAS_LIST);
 
-        for(String mas : MAS_LIST){
-            WebElement course = webDriver.findElement(By.linkText(mas));
-            masterUrls.add(course.getAttribute("href"));
-        }
+        List<String> elementaryYearsUrls = getUrlsByPartialLinkText(elementaryUrls, PARTIAL_LINK_YEAR);
+        List<String> elementarySemesterUrls = getUrlsByPartialLinkText(elementaryYearsUrls, PARTIAL_LINK_SEMESTER);
 
-        for(String elementaryUrl : elementaryUrls){
-            webDriver.get(elementaryUrl);
-
-            List<WebElement> years = webDriver.findElements(By.partialLinkText(" godina"));
-            for(WebElement year : years){
-                elementaryYearsUrls.add(year.getAttribute("href"));
-            }
-        }
-
-        for (String yearUrl : elementaryYearsUrls){
-            webDriver.get(yearUrl);
-
-            List<WebElement> semesters = webDriver.findElements(By.partialLinkText(" semestar"));
-            for(WebElement semester : semesters){
-                elementarySemesterUrls.add(semester.getAttribute("href"));
-            }
-        }
-
-        for(String elementarySemesterUrl : elementarySemesterUrls){
-            webDriver.get(elementarySemesterUrl);
-
-            List<WebElement> semesterCourses = webDriver.findElements(By.className("aalink"));
-
-            for(WebElement course : semesterCourses){
-                courses.add(
-                        Course.builder()
-                                .name(course.getText())
-                                .url(course.getAttribute("href"))
-                                .build()
-                );
-            }
-        }
+        List<Course> courses = extractCourses(elementarySemesterUrls);
 
         for (Course course : courses){
             populateCourseProfessor(course);
@@ -135,14 +77,81 @@ public class SeleniumServiceImpl implements SeleniumService {
         return courses;
     }
 
+    private List<Course> extractCourses(List<String> elementarySemesterUrls) {
+        List<Course> courses = new ArrayList<>();
+
+        for(String elementarySemesterUrl : elementarySemesterUrls) {
+            webDriver.get(elementarySemesterUrl);
+
+            List<WebElement> semesterCourses = webDriver.findElements(By.className("aalink"));
+
+            for(WebElement course : semesterCourses){
+                courses.add(
+                        Course.builder()
+                                .name(course.getText())
+                                .url(course.getAttribute(HREF_TAG))
+                                .build()
+                );
+            }
+        }
+
+        return courses;
+    }
+
+    private List<String> getUrlsByPartialLinkText(List<String> elementaryUrls, String partialLinkText) {
+        List<String> urls = new ArrayList<>();
+
+        for(String elementaryUrl : elementaryUrls){
+            webDriver.get(elementaryUrl);
+
+            List<WebElement> years = webDriver.findElements(By.partialLinkText(partialLinkText));
+            for(WebElement year : years){
+                urls.add(year.getAttribute(HREF_TAG));
+            }
+        }
+
+        return urls;
+    }
+
+    private List<String> getDepartmentUrls(List<String> departments) {
+        List<String> urls = new ArrayList<>();
+
+        for(String department : departments){
+            WebElement course = webDriver.findElement(By.linkText(department));
+            urls.add(course.getAttribute(HREF_TAG));
+        }
+
+        return urls;
+    }
+
+    private List<Employee> extractEmployees(List<WebElement> employeeRows) {
+        List<Employee> employees = new ArrayList<>();
+
+        for (WebElement employeeRow : employeeRows) {
+            employees.add(
+                    Employee.builder()
+                            .name(getCellPropertyByClassName(employeeRow, EMPLOYEE_CELL_NAME))
+                            .title(getCellPropertyByClassName(employeeRow, EMPLOYEE_CELL_TITLE))
+                            .cabinet(getCellPropertyByClassName(employeeRow, EMPLOYEE_CELL_CABINET))
+                            .locale(getCellPropertyByClassName(employeeRow, EMPLOYEE_CELL_LOCALE))
+                            .email(getCellPropertyByClassName(employeeRow, EMPLOYEE_CELL_EMAIL))
+                            .consultation(getCellPropertyByClassName(employeeRow, EMPLOYEE_CELL_CONSULTATION))
+                            .build()
+            );
+        }
+
+        webDriver.quit();
+        return employees;
+    }
+
     private void populateCourseProfessor(Course course){
         webDriver.get(course.getUrl());
-        if(isElementPresent(webDriver, By.xpath(".//*[text()[contains(.,\"Predmetni nastavn\")]]"))){
+        if(isElementPresent(By.xpath(".//*[text()[contains(.,\"Predmetni nastavn\")]]"))){
             WebElement element = webDriver.findElement(By.xpath(".//*[text()[contains(.,\"Predmetni nastavn\")]]"));
             course.setProfessor(element.getText());
 
             if (element.getText().equals("Predmetni nastavnik:")) {
-                if(isElementPresent(webDriver, By.xpath(".//*[text()[contains(.,\"Predmetni nastavn\")]]/following-sibling::*[1]"))) {
+                if(isElementPresent(By.xpath(".//*[text()[contains(.,\"Predmetni nastavn\")]]/following-sibling::*[1]"))) {
                     element = webDriver.findElement(By.xpath(".//*[text()[contains(.,\"Predmetni nastavn\")]]/following-sibling::*[1]"));
                     course.setProfessor(element.getText());
                 }
@@ -150,7 +159,7 @@ public class SeleniumServiceImpl implements SeleniumService {
 
             if (element.getText().equals("Predmetni nastavnik:")) {
                 System.out.println(element.getAttribute("innerHTML"));
-                if(isElementPresent(webDriver, By.xpath(".//*[text()[contains(.,\"Predmetni nastavn\")]]/following-sibling::b[1]/a"))) {
+                if(isElementPresent(By.xpath(".//*[text()[contains(.,\"Predmetni nastavn\")]]/following-sibling::b[1]/a"))) {
                     element = webDriver.findElement(By.xpath(".//*[text()[contains(.,\"Predmetni nastavn\")]]/following-sibling::b[1]/a"));
                     System.out.println(element.getText());
                 }
@@ -159,12 +168,7 @@ public class SeleniumServiceImpl implements SeleniumService {
     }
 
     private String getCellPropertyByClassName(WebElement employeeRow, String className) {
-        WebElement cell = null;
-
-        if (isElementPresent(employeeRow, By.className(className))){
-            cell = employeeRow.findElement(By.className(className));
-        }
-
+        WebElement cell = isElementPresent(employeeRow, By.className(className)) ? employeeRow.findElement(By.className(className)) : null;
         return cell != null ? cell.getText() : null;
     }
 
@@ -177,7 +181,7 @@ public class SeleniumServiceImpl implements SeleniumService {
         }
     }
 
-    private boolean isElementPresent(WebDriver webDriver, By by) {
+    private boolean isElementPresent(By by) {
         try{
             webDriver.findElement(by);
             return true;
